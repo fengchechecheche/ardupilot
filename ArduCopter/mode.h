b817340aff29c52458c7912f3442b04cc2ba2d84
fengchechecheche <2447023657@qq.com>
2024-01-06 23:38:26 +0800

    增加了五角星航线模式的枚举序号，增加五角星航线模式的类定义

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -39,6 +39,7 @@ public:
         AUTOROTATE =   26,  // Autonomous autorotation
         AUTO_RTL =     27,  // Auto RTL, this is not a true mode, AUTO will report as this mode if entered to perform a DO_LAND_START Landing sequence
         TURTLE =       28,  // Flip over after crash
+        DRAWSTAR =     29,  // 五角星航线模式
     };
 
     // constructor
@@ -816,6 +817,44 @@ private:
   class to support FLOWHOLD mode, which is a position hold mode using
   optical flow directly, avoiding the need for a rangefinder
  */
+
+
+class ModeDrawStar : public Mode {
+
+public:
+    // inherit constructor
+    using Mode::Mode;
+    //注：Copter4.0.7的代码少写了下面这个语句，导致编译报错
+    // error: cannot declare field ‘Copter::mode_drawstar’ to be of abstract type ‘ModeDrawStar’
+    Number mode_number() const override { return Number::DRAWSTAR; }
+
+    bool init(bool ignore_checks) override;
+    void run() override;
+
+    bool requires_GPS() const override { return true; }  // 此模式需要有GPS定位
+    bool has_manual_throttle() const override { return false; }  // 此模式不允许手动控制油门
+    // bool allows_arming(bool from_gcs) const override { return false; }  // 不允许在此模式下解锁
+    //注：4.0.7中的代码在此处报错，仿照其他模式的代码进行了修改。
+    bool allows_arming(AP_Arming::Method method) const override { return true; };
+    bool is_autopilot() const override { return true; }  // 此模式为自动飞行控制
+    bool has_user_takeoff(bool must_navigate) const override { return false; }  // 不允许在此模式下直接起飞（必须是在空中切到此模式）
+    bool in_guided_mode() const override { return true; }  // 此模式是一种引导的模式
+
+protected:
+
+    const char *name() const override { return "DRAW_STAR"; }
+    const char *name4() const override { return "STAR"; }
+
+private:
+    Vector3f path[10];  // 航点数组
+    int path_num;  // 当前航点号
+
+    void generate_path();  // 生成航线
+    void pos_control_start();  // 开始位置控制
+    void pos_control_run();  // 位置控制周期调用函数
+
+};
+
 
 class ModeFlowHold : public Mode {
 public:
