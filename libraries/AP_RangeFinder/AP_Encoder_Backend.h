87f66017183132f3ed1724fb405757aae8aae68d
fengchechecheche <2447023657@qq.com>
2025-06-06 16:31:10 +0800

    【04】做了一些无关紧要的小修改

--- libraries/AP_RangeFinder/AP_Encoder_Backend.h
+++ libraries/AP_RangeFinder/AP_Encoder_Backend.h
@@ -11,6 +11,7 @@ class AP_Encoder_Backend{
 public:
     // constructor. This incorporates initialisation as well.
 	AP_Encoder_Backend(AP_Encoder& encoder);
+    ~AP_Encoder_Backend(){};
 
     // update the state structure
     virtual void update() = 0;
