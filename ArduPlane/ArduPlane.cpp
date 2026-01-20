63c94dcb4f8deeaf529bad04fc8836f666a5c789
fengchechecheche <72700523+fengchechecheche@users.noreply.github.com>
2026-01-20 17:32:38 +0800

    [ 02 ] 提高 RPM 传感器刷新频率到 100Hz

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -94,7 +94,7 @@ const AP_Scheduler::Task Plane::scheduler_tasks[] = {
     SCHED_TASK(three_hz_loop,           3,     75,  93),
     SCHED_TASK(check_long_failsafe,     3,    400,  96),
 #if AP_RPM_ENABLED
-    SCHED_TASK_CLASS(AP_RPM,           &plane.rpm_sensor,     update,     10, 100,  99),
+    SCHED_TASK_CLASS(AP_RPM,           &plane.rpm_sensor,     update,     100, 100,  99),
 #endif
 #if AP_AIRSPEED_AUTOCAL_ENABLE
     SCHED_TASK(airspeed_ratio_update,   1,    100,  102),
